ba55d7d75def7e9e31099277bd090178301f1cdc
Nick Palladinos <nick.palladinos@gmail.com>
2025-05-04 23:31:14 +0300

    Update Repmin

--- src/Repmin.lean
+++ src/Repmin.lean
@@ -13,26 +13,26 @@ unsafe def trace : (a → Thunk b → (c × b)) → a → c :=
 
 
 inductive Tree (a : Type u) : Type u
-  | Leaf : a → Tree a
-  | Branch : Tree a → Tree a → Tree a
+  | lead : a → Tree a
+  | branch : Tree a → Tree a → Tree a
 
 def copy : Tree Nat → Thunk Nat → (Tree (Thunk Nat) × Nat) :=
   fun t thunk =>
     match t with
-    | .Leaf x => (.Leaf thunk, x)
-    | .Branch l r =>
+    | .lead x => (.lead thunk, x)
+    | .branch l r =>
       let (tl, ml) := copy l thunk
       let (tr, mr) := copy r thunk
-      (.Branch tl tr, min ml mr)
+      (.branch tl tr, min ml mr)
 
 unsafe def repmin : Tree Nat → Tree (Thunk Nat) :=
   fun t => trace copy t
 
 def print : Tree (Thunk Nat) → String
-  | .Leaf x => s!"L {x.get}"
-  | .Branch l r => s!"(B {print l} {print r})"
+  | .lead x => s!"L {x.get}"
+  | .branch l r => s!"(B {print l} {print r})"
 
-def test : Tree Nat := .Branch (.Branch (.Leaf 0) (.Leaf 2)) (.Leaf 1)
+def test : Tree Nat := .branch (.branch (.lead 0) (.lead 2)) (.lead 1)
 
 #eval test |> repmin |> print
 
@@ -43,15 +43,15 @@ def test : Tree Nat := .Branch (.Branch (.Leaf 0) (.Leaf 2)) (.Leaf 1)
 def repmin' : Tree Nat → Tree Nat :=
   fun t =>
     let rec aux : Tree Nat → ((Nat → Tree Nat) × Nat)
-      | .Leaf x => (fun m => .Leaf m, x)
-      | .Branch l r =>
+      | .lead x => (fun m => .lead m, x)
+      | .branch l r =>
         let (fl, ml) := aux l
         let (fr, mr) := aux r
-        (fun m => .Branch (fl m) (fr m), min ml mr)
+        (fun m => .branch (fl m) (fr m), min ml mr)
 
     let (f, m) := aux t
     f m
 
 
-example : repmin' (.Branch (.Branch (.Leaf 0) (.Leaf 2)) (.Leaf 1)) = .Branch (.Branch (.Leaf 0) (.Leaf 0)) (.Leaf 0) :=
+example : repmin' (.branch (.branch (.lead 0) (.lead 2)) (.lead 1)) = .branch (.branch (.lead 0) (.lead 0)) (.lead 0) :=
   rfl
