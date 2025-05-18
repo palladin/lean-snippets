0ac1fa9f50b28dec6d28f8858ac90e37690242cb
Nick Palladinos <nick.palladinos@gmail.com>
2025-05-18 15:05:07 +0300

    Update lazy macro

--- Snippets/MyMacros.lean
+++ Snippets/MyMacros.lean
@@ -1,8 +1,9 @@
 
 
-syntax "lazy" "(" term ")" : term
+syntax:min "lazy" "(" term ")" : term
 macro_rules
   | `(lazy ($x)) => `(Thunk.mk (fun () => $x))
 
 
 #check lazy (1 + 1)
+#check Thunk.get (lazy (1 + 1))
