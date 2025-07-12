e17a5d89104f4c29679578a25a547cfca689ca71
Nick Palladinos <nick.palladinos@gmail.com>
2025-07-12 16:38:50 +0300

    Update Unlambda to handle delay

--- Snippets/Unlambda.lean
+++ Snippets/Unlambda.lean
@@ -2,21 +2,25 @@ import Snippets.ContT
 
 -- Based on http://www.madore.org/~david/programs/unlambda/
 
-def Rec := Dynamic → ContT Unit IO Dynamic
+inductive Value where
+  | Delay : Value
+  | Func : (Dynamic → ContT Unit IO Dynamic) → Value
 
-deriving instance TypeName for Rec
 
-def get : Dynamic → IO Rec := fun d =>
-  match d.get? Rec with
+deriving instance TypeName for Value
+
+def get : Dynamic → IO Value := fun d =>
+  match d.get? Value with
   | .some r => pure r
-  | .none => panic! "Invalid Rec!"
+  | .none => panic! "Invalid Value!"
 
-def toRec : (Rec → ContT Unit IO Rec) → Rec := fun k d => do
-  let r ← get d
-  let r' ← k r
-  pure <| Dynamic.mk r'
+def toValue : (Value → ContT Unit IO Value) → Value := fun k =>
+  Value.Func (fun d => do
+    let r ← get d
+    let r' ← k r
+    pure <| Dynamic.mk r')
 
-def liftRec : (Rec → ContT Unit IO Rec) → ContT Unit IO Rec := fun f => f |> toRec |> pure
+def liftValue : (Value → ContT Unit IO Value) → ContT Unit IO Value := fun f => pure (toValue f)
 
 inductive Expr where
   | s : Expr
@@ -24,36 +28,51 @@ inductive Expr where
   | i : Expr
   | r : Expr
   | c : Expr
+  | d : Expr
   | dot : Char → Expr
   | backtick : Expr → Expr → Expr
 
-def applyRec : Rec → Rec → ContT Unit IO Rec := fun f r => do
-  let r' ← f (Dynamic.mk r)
-  get r'
+def applyValue : Value → Value → ContT Unit IO Value := fun f arg => do
+  match f with
+  | Value.Func func => do
+    let result ← func (Dynamic.mk arg)
+    get result
+  | Value.Delay => pure Value.Delay
 
-def apply : ContT Unit IO Rec → ContT Unit IO Rec → ContT Unit IO Rec := fun first second => do
+def apply : ContT Unit IO Value → ContT Unit IO Value → ContT Unit IO Value := fun first second => do
   let f ← first
   let s ← second
-  let result ← f (Dynamic.mk s)
-  get result
+  applyValue f s
 
-def eval : Expr → (Char → IO Unit) → ContT Unit IO Rec := fun expr put =>
+def eval : Expr → (Char → IO Unit) → ContT Unit IO Value := fun expr put =>
   match expr with
-  | .s => liftRec (fun rec1 =>
-               liftRec (fun rec2 =>
-               liftRec (fun rec3 =>
+  | .s => liftValue (fun rec1 =>
+               liftValue (fun rec2 =>
+               liftValue (fun rec3 =>
                apply (apply (pure rec1) (pure rec3)) (apply (pure rec2) (pure rec3)))))
-  | .k => liftRec (fun rec1 =>
-               liftRec (fun _ => pure rec1))
-  | .i => liftRec (fun rec => pure rec)
-  | .r => liftRec (fun rec => do
+  | .k => liftValue (fun rec1 =>
+               liftValue (fun _ => pure rec1))
+  | .i => liftValue (fun rec => pure rec)
+  | .r => liftValue (fun rec => do
                put '\n'
                pure rec)
-  | .c => liftRec (fun rec => callCC (fun k => applyRec rec (toRec k)))
-  | .dot char => liftRec (fun rec => do
+  | .c => liftValue (fun rec => callCC (fun k => applyValue rec (toValue k)))
+  | .d => pure Value.Delay
+  | .dot char => liftValue (fun rec => do
                put char
                pure rec)
-  | .backtick expr expr' => apply (eval expr put) (eval expr' put)
+  | .backtick expr expr' => do
+      let f ← eval expr put
+      match f with
+      | Value.Delay =>
+          pure (Value.Func (fun d => do
+            let arg ← get d
+            let func ← eval expr' put
+            let result ← applyValue func arg
+            pure (Dynamic.mk result)))
+      | _ =>
+          let arg ← eval expr' put
+          applyValue f arg
 
 partial def parse : List Char → Option (Expr × List Char) := fun chars =>
   match chars with
@@ -62,6 +81,7 @@ partial def parse : List Char → Option (Expr × List Char) := fun chars =>
   | 'i' :: xs => some (.i, xs)
   | 'r' :: xs => some (.r, xs)
   | 'c' :: xs => some (.c, xs)
+  | 'd' :: xs => some (.d, xs)
   | '.' :: c :: xs => some (.dot c, xs)
   | '`' :: xs => do
       let (expr, xs') ← parse xs
@@ -82,6 +102,14 @@ def callCCInfinite : String :=
 def helloWorldTest : String :=
   "```si`k``s.H``s.e``s.l``s.l``s.o``s. ``s.w``s.o``s.r``s.l``s.d``s.!``sri``si``si``si``si``si``si``si``si`ki"
 
+def helloWorldInfinite : String :=
+  "```s``sii`ki``s``s`ks``s``s`ks``s`k`s`kr``s`k`si``s`k`s`k`d````````````.H.e.l.l.o.,. .w.o.r.l.d.!kk`k``s``s`ksk`k.*"
+
+def delayExample1 : String :=
+  "``d`rii" -- prints a blank line
+
+def delayExample2 : String :=
+  "`id`ri" -- does not print a blank line
 
 def runExample : String → IO Unit := fun program => do
   match parse program.data with
@@ -90,13 +118,26 @@ def runExample : String → IO Unit := fun program => do
       pure ()
   | none => IO.println "Parse error"
 
-def main : List String → IO UInt32 := fun _ => do
-  IO.println "Running hello world test..."
-  runExample helloWorldTest
+def delayExamples : IO Unit := do
+  IO.println "Running delay example 1..."
+  runExample delayExample1
+  IO.println "Running delay example 2..."
+  runExample delayExample2
+
+def callCCExamples : IO Unit := do
   IO.println "Running callCC test..."
   runExample callCCTest
   IO.println "Running callCC numbers test..."
   runExample callCCNumbers
   --IO.println "Running callCC infinite test..."
   --runExample callCCInfinite
+
+def helloWorldExample : IO Unit := do
+  IO.println "Running hello world test..."
+  runExample helloWorldTest
+  runExample helloWorldInfinite
+
+def main : List String → IO UInt32 := fun _ => do
+  delayExamples
+  helloWorldExample
   return 0
