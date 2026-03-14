c98a153d6356ad430f952d8608b450767268dd3a
Nick Palladinos <nick.palladinos@gmail.com>
2026-03-14 19:22:23 +0200

    Extend the SystemFPHOAS example with Church-encoded polymorphic lists.
    - define the Church list type and basic list constructors
    - add explicit type application for polymorphic term instantiation
    - implement map for Church-encoded lists
    - add eval and pretty-print examples for the new terms

--- Snippets/SystemFPHOAS.lean
+++ Snippets/SystemFPHOAS.lean
@@ -59,11 +59,35 @@ def churchNatTy : Ty :=
     .fn (.fn (.var a) (.var a))
       (.fn (.var a) (.var a)))
 
+def churchListBody {tvar : Type _} (a : tvar) : Ty' tvar :=
+  .all (fun r =>
+    .fn (.var r)
+      (.fn (.fn (.var a) (.fn (.var r) (.var r)))
+        (.var r)))
+
+def churchListTy : Ty :=
+  .all churchListBody
+
+def churchSingletonTy : Ty :=
+  .all (fun a =>
+    .fn (.var a) (churchListBody a))
+
+def churchPairTy : Ty :=
+  .all (fun a =>
+    .fn (.var a) (.fn (.var a) (churchListBody a)))
+
+def churchListMapTy : Ty :=
+  .all (fun a =>
+    .all (fun b =>
+      .fn (.fn (.var a) (.var b))
+        (.fn (churchListBody a) (churchListBody b))))
+
 inductive Term' {tvar : Type _} (rep : Ty' tvar -> Type _) : Ty' tvar -> Type _ where
   | var : rep ty -> Term' rep ty
   | lam : (rep dom -> Term' rep ran) -> Term' rep (.fn dom ran)
   | app : Term' rep (.fn dom ran) -> Term' rep dom -> Term' rep ran
   | tlam : ((ty : tvar) -> Term' rep (body ty)) -> Term' rep (.all body)
+  | tapp : Term' rep (.all body) -> (ty : tvar) -> Term' rep (body ty)
 
 abbrev Term (ty : Ty) := {tvar : Type _} -> {rep : Ty' tvar -> Type _} -> Term' rep (ty (tvar := tvar))
 
@@ -74,20 +98,26 @@ def pretty {ty : Ty' String} (term : Term' (fun _ : Ty' String => String) ty)
   match term with
   | .var name => name
   | .lam body =>
-      let name := s!"x{nextTm}"
-      s!"(fun {name} => {pretty (body name) nextTy (nextTm + 1)})"
+    let name := s!"x{nextTm}"
+    s!"(fun {name} => {pretty (body name) nextTy (nextTm + 1)})"
   | .app funTerm argTerm =>
-      s!"({pretty funTerm nextTy nextTm} {pretty argTerm nextTy nextTm})"
+    s!"({pretty funTerm nextTy nextTm} {pretty argTerm nextTy nextTm})"
   | .tlam body =>
-      let name := s!"T{nextTy}"
-      s!"(Lambda {name}. {pretty (body name) (nextTy + 1) nextTm})"
+    let name := s!"T{nextTy}"
+    s!"(Lambda {name}. {pretty (body name) (nextTy + 1) nextTm})"
+  | .tapp (body := body) funTerm tyArg =>
+    let _ := body
+    s!"({pretty funTerm nextTy nextTm} [{tyArg}])"
 
 @[simp] def denote {ty : Ty' (Type v)} : Term' Ty'.denote ty -> Ty'.denote ty
   | .var x => x
   | .lam body => show Ty'.denote (.fn _ _) from fun x => denote (body x)
   | .app funTerm argTerm =>
-      show Ty'.denote _ from (denote funTerm) (denote argTerm)
+    show Ty'.denote _ from (denote funTerm) (denote argTerm)
   | .tlam body => show Ty'.denote (.all _) from fun ty => denote (body ty)
+  | .tapp (body := body) funTerm tyArg =>
+    let _ := body
+    show Ty'.denote _ from (denote funTerm) tyArg
 
 end Term'
 
@@ -140,6 +170,49 @@ def churchTwo : Term churchNatTy :=
       .lam (fun x =>
         .app (.var f) (.app (.var f) (.var x)))))
 
+def churchNil : Term churchListTy :=
+  .tlam (fun _ =>
+    .tlam (fun _ =>
+      .lam (fun z =>
+        .lam (fun _ =>
+          .var z))))
+
+def churchSingleton : Term churchSingletonTy :=
+  .tlam (fun _ =>
+    .lam (fun x =>
+      .tlam (fun _ =>
+        .lam (fun z =>
+          .lam (fun cons =>
+            .app (.app (.var cons) (.var x)) (.var z))))))
+
+def churchPair : Term churchPairTy :=
+  .tlam (fun _ =>
+    .lam (fun x =>
+      .lam (fun y =>
+        .tlam (fun _ =>
+          .lam (fun z =>
+            .lam (fun cons =>
+              .app (.app (.var cons) (.var x))
+                (.app (.app (.var cons) (.var y)) (.var z))))))))
+
+def churchListMap : Term churchListMapTy :=
+  .tlam (fun a =>
+    .tlam (fun _ =>
+      .lam (fun f =>
+        .lam (fun xs =>
+          .tlam (fun r =>
+            .lam (fun z =>
+              .lam (fun cons =>
+                let step : Term' _ (.fn (.var a) (.fn (.var r) (.var r))) :=
+                  .lam (fun x =>
+                    .lam (fun acc =>
+                      .app
+                        (.app (.var cons) (.app (.var f) (.var x)))
+                        (.var acc)))
+                .app
+                  (.app (.tapp (.var xs) r) (.var z))
+                  step)))))))
+
 #check show (x : Type) → ULift x → ULift x from Term.denote polyId
 
 #eval (Term.denote polyId) Nat ⟨3⟩
@@ -148,15 +221,28 @@ def churchTwo : Term churchNatTy :=
 #eval (Term.denote churchZero) Nat (fun n => ⟨n.down + 1⟩) ⟨0⟩
 #eval (Term.denote churchOne) Nat (fun n => ⟨n.down + 1⟩) ⟨0⟩
 #eval (Term.denote churchTwo) Nat (fun n => ⟨n.down + 1⟩) ⟨0⟩
+#eval (Term.denote churchNil) Nat Nat ⟨0⟩ (fun _ tail => tail)
+#eval (Term.denote churchSingleton) Nat ⟨7⟩ Nat ⟨0⟩ (fun _ tail => ⟨tail.down + 1⟩)
+#eval (Term.denote churchPair) Nat ⟨7⟩ ⟨11⟩ Nat ⟨0⟩ (fun _ tail => ⟨tail.down + 1⟩)
+#eval (Term.denote churchListMap) Nat Nat (fun n => ⟨n.down + 10⟩)
+  ((Term.denote churchPair) Nat ⟨7⟩ ⟨11⟩) Nat ⟨0⟩ (fun x tail => ⟨x.down + tail.down⟩)
 
 #eval Ty.pretty idTy
 #eval Ty.pretty composeTy
 #eval Ty.pretty churchNatTy
+#eval Ty.pretty churchListTy
+#eval Ty.pretty churchSingletonTy
+#eval Ty.pretty churchPairTy
+#eval Ty.pretty churchListMapTy
 #eval Term.pretty polyId
 #eval Term.pretty polyConst
 #eval Term.pretty compose
 #eval Term.pretty churchZero
 #eval Term.pretty churchOne
 #eval Term.pretty churchTwo
+#eval Term.pretty churchNil
+#eval Term.pretty churchSingleton
+#eval Term.pretty churchPair
+#eval Term.pretty churchListMap
 
 end SystemFPHOAS
